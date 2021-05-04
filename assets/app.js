c4deec3dcef7727c549d00ff4b05c2029ac9e233
Sean Simpson <simpson.sean@outlook.com>
2021-05-04 16:55:18 -0700

    First Round of code

---
+++ assets/app.js
@@ -0,0 +1,21 @@
+// import functions and grab DOM elements
+import function getRandomNum() {
+    
+}
+
+const userInput = document.getElementById('user-input');
+const subButton = document.getElementById('sub-button');
+const compNumber = Math.ceil(Math.random() *20);
+
+// initialize state
+
+//functions
+
+// set event listeners to update state and DOM
+subButton.addEventListener('click', () => {
+    //Console log to validate button click
+    console.log(compNumber);
+
+
+
+});
---
+++ assets/utils.js
@@ -0,0 +1,15 @@
+//Functions
+
+export function compareNumber(userGuess) {
+    if (userGuess) {
+
+    }
+
+
+}
+
+function getRandomNum (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * 20);
+}
